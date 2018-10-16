0c5ec42a4d746c99f59fab4df3fcc21223f7bd10
MontgomeryT <thomas.montgomery@austintexas.gov>
2018-10-16 14:47:57 -0500

    clarified names and comments

--- python/extract_metadata.py
+++ python/extract_metadata.py
@@ -3,34 +3,36 @@ import requests
 # import json
 # import xmlrpc.client
 
-datasets = []
+assets = []
 count = 0
 
 
 def gather_all_assets():
-    """Gathers datasets from socrata for metadata generation and compiles a list."""
+    """Gathers assets from socrata for metadata generation and compiles a list."""
     global count
-    global datasets
+    global assets
     # limits query to 2000 results
     url = 'http://api.us.socrata.com/api/catalog/v1?limit=2000&domains=data.austintexas.gov'
     r = requests.get(url).json()
 
     for item in r['results']:
         try:
-            datasets.append({item['resource']['id']: (item['resource']['name'], # name
-                                                      item['classification']['domain_metadata'][0]['value'], # department
-                                                      item['resource']['type'], # type
-                                                      item['resource']['createdAt'].split('-')[0], # publication year
-                                                      item['permalink'], # permalink
-                                                      item['resource']['description'] # description
-                                                     )
+            assets.append({item['resource']['id']: (item['resource']['name'],  # name
+                                                    item['classification']['domain_metadata'][0]['value'],  # department
+                                                    item['resource']['type'],  # type
+                                                    item['resource']['createdAt'].split('-')[0],  # publication year
+                                                    item['permalink'],  # permalink
+                                                    item['resource']['description']  # description
+                                                    )
                             }
                            )
         except IndexError:
             count +=1
-    return datasets
+    return assets
 
 
 if __name__ == "__main__":
-    assets = gather_all_assets()
-    print(assets[50])
+    result_assets = gather_all_assets()
+    print("{} assets do not contain metadata".format(count))
+    for asset in result_assets[:5]:
+        print(asset)
