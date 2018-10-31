bdb7ca36db639477e607c722ab962a041e959c46
MontgomeryT <thomas.montgomery@austintexas.gov>
2018-10-31 12:19:50 -0500

    added commentary

--- doi-automation/extract_metadata.py
+++ doi-automation/extract_metadata.py
@@ -10,7 +10,8 @@ count = 0
 
 
 def gather_assets():
-    """Gathers assets from socrata for metadata generation and compiles a dictionary of tuples."""
+    """Gathers assets from socrata for metadata generation and returns a list of dictionaries. Dictionaries keys are
+    field names as required by DataCite and values are from Socrata"""
     global count
     global assets
     # limits query to 2000 results
@@ -19,28 +20,34 @@ def gather_assets():
 
     for item in r['results']:
         try:
+            # retrieve department name by looking for field with Department in name. Domain metadata field order varies
             for dictionary in item['classification']['domain_metadata']:
                 for key in dictionary.keys():
                     if 'Department' in dictionary['key']:
                         department = dictionary['value']
+            # assemble dictionary and append to asset list
             if item['resource']['type'] == 'dataset':
                 assets.append({'socrata_4x4': item['resource']['id'], 'name': item['resource']['name'],
                                'department': department,
-                               'type': item['resource']['type'],  'year': item['resource']['createdAt'].split('-')[0],
-                               'permalink': item['permalink'], 'desc': item['resource']['description']})
+                               'type': item['resource']['type'],
+                               'year': item['resource']['createdAt'].split('-')[0],
+                               'permalink': item['permalink'],
+                               'desc': item['resource']['description']})
 
         except IndexError:
+            # count assets that do not have fields above by throwing an IndexError
             count +=1
     return assets
 
 
 def load_db():
+    """Load list of dictionaries into temp table in database for diff"""
+    # use env variables for credentials
     conn = psycopg2.connect(host="localhost", database="citation-station", user=os.environ['postgres_user'],password=os.environ['postgres_pass'])
     cur = conn.cursor()
 
 
 if __name__ == "__main__":
-    import csv
     result_assets = gather_assets()
     print(len(result_assets))
     print("{} assets do not contain metadata".format(count))
