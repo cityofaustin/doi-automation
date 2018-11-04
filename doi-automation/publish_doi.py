d1c7c6f1eebf3188f6f61d40e1fef3111198a84e
MontgomeryT <thomas.montgomery@austintexas.gov>
2018-11-04 12:47:41 -0600

    started writing to xml elements to map metadata and create temp xml file for insertion into postgres and conversion to base64 for payload

--- doi-automation/publish_doi.py
+++ doi-automation/publish_doi.py
@@ -1,6 +1,8 @@
-import psycopg2
-# import requests
 import os
+# import requests
+# import base64
+
+import psycopg2
 
 
 def assemble_payload(socrata_4x4):
@@ -27,19 +29,25 @@ def assemble_payload(socrata_4x4):
     select_department_dois = """SELECT * FROM doi_assets WHERE department = '{}'""".format(metadata[0][2])
     cur.execute(select_department_dois)
     department_dois = cur.fetchall()
-
     suffix_list = []
     for doi in department_dois:
         suffix_list.append(int(doi[6]))
-
+    # add 1 to highest suffix value and use zfill to pad w/zeros to 6 chars
     doi_suffix = str((max(suffix_list)+1)).zfill(6)
+    identifier = '10.26000/{}.{}'.format(dept_prefix[0][0], doi_suffix)
 
     payload = {'data':
                 {'type':
                     'dois',
                     'attributes':
                         {'doi':
-                            '10.26000/{}.{}'.format(dept_prefix[0][0], doi_suffix)},
+                            '{}'.format(identifier),
+                         'url':
+                            '{}'.format(metadata[0][5]),
+                         'xml':
+                            ''
+                         },
+
                     'relationships':
                         {'client':
                             {'data':
@@ -50,9 +58,52 @@ def assemble_payload(socrata_4x4):
                          }
                  }
                }
-    return payload
+    return identifier, payload, metadata
+
+
+def assemble_xml(metadata, doi_identifier):
+    import xml.etree.ElementTree as ET
+    datacite_example = '{}\\xml\\datacite-example.xml'.format((os.path.dirname(os.path.realpath(__file__))))
+
+    # get directory to find xml example
+    tree = ET.parse(datacite_example)
+    root = tree.getroot()
+    resource_id = root[0].tag.split('}')[0].lstrip('{')
+    # set doi identifier
+    for node in root.iter('{http://datacite.org/schema/kernel-3}identifier'):
+        node.text = str(doi_identifier)
+    # set title
+    for node in root.iter('{http://datacite.org/schema/kernel-3}title'):
+        node.text = metadata[0][1]
+    # set pub year
+    for node in root.iter('{http://datacite.org/schema/kernel-3}publicationYear'):
+        node.text = str(metadata[0][4])
+    # set creator/dept
+    for node in root.iter('{http://datacite.org/schema/kernel-3}creator'):
+        node.text = metadata[0][2]
+    # set description
+    for node in root.iter('{http://datacite.org/schema/kernel-3}description'):
+        node.text = metadata[0][6]
+    output_xml = ('{}\\xml\\datacite-test.xml'.format((os.path.dirname(os.path.realpath(__file__)))))
+    # create temp output
+    tree.write(output_xml)
+
+    tree = ET.parse(output_xml)
+    root = tree.getroot()
+    # tree.find('resource/identifier').text = '{}.{}'.format(prefix, suffix)
+    # tree = lxml.etree.parse('{}\\xml\\datacite-example.xml'.format((os.path.dirname(os.path.realpath(__file__)))))
+    # root = tree.getroot()
+    return root
+
+
+def publish_doi(xml, payload, draft=True):
+    pass
 
 
 if __name__ == "__main__":
-    output = assemble_payload('jhra-82n2')
-    print(output)
+    identifier, payload, metadata = assemble_payload('jhra-82n2')
+    output = assemble_xml(metadata, identifier)
+    for node in output.iter():
+        print(node.tag)
+        print(node.text)
+        # print(node.attrib)
